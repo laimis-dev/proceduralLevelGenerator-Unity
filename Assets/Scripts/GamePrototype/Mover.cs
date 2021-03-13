1d0c38b751700ae1d65260c1b306f790077ccfe3
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2021-03-13 23:52:44 +0200

    making player

--- Assets/Scripts/GamePrototype/Mover.cs
+++ Assets/Scripts/GamePrototype/Mover.cs
@@ -14,10 +14,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButton(0)){
             MoveToCursor();
         }
-        
+        UpdateAnimator();
     }
 
     private void MoveToCursor(){
@@ -27,5 +27,12 @@ public class Mover : MonoBehaviour
         if(hasHit){
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator(){
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
     }
 }
