958125225246a787b898582ce8616ad6f61cf2a7
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2021-03-19 12:27:38 +0200

    cinemachine

--- Assets/Scripts/GamePrototype/Control/AIController.cs
+++ Assets/Scripts/GamePrototype/Control/AIController.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 
 
 namespace RPG.Control{
     public class AIController : MonoBehaviour {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
         Fighter fighter;
         Health health;
+        Mover mover;
         GameObject player;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start(){
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
         }
         private void Update(){
             if(health.IsDead()) return;
             if(InAttackRange() && fighter.CanAttack(player)){
+                timeSinceLastSawPlayer = 0;
                 fighter.Attack(player);
                 // print("chase");
+            } else if (timeSinceLastSawPlayer < suspicionTime){
+                GetComponent<ActionScheduler>().CancelCurrentAction();
             } else {
-                fighter.Cancel();
+                mover.StartMoveAction(guardPosition);
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
         private bool InAttackRange(){
